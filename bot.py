b418da752ba757620953e443ee234ca7ce762738
bozzyk <bozzyk44@gmail.com>
2021-11-07 16:37:57 +0300

    Update bot.py to do some basic actions

--- bot.py
+++ bot.py
@@ -7,10 +7,12 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+class MyClient(discord.Client):
 
+    async def on_message(self, msg):
+        print(msg.content)
+
+
+client = MyClient()
 client.run(TOKEN)
